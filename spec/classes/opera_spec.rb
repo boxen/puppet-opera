3e5f0e6dcd357fb37cc46ea395a10c419cc58206
Peter Rhoades <pete@simpleweb.co.uk>
2013-07-11 13:18:32 +0100

    Updating spec to test for new provider

--- spec/classes/opera_spec.rb
+++ spec/classes/opera_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'opera' do
   it do
     should contain_package('Opera').with({
-      :provider => 'appdmg',
+      :provider => 'appdmg_eula',
       :source   => 'http://get.geo.opera.com/pub/opera/desktop/15.0.1147.132/mac/Opera_15.0.1147.132_Setup.dmg',
     })
   end
