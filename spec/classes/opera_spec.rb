6e1af1b3c88bfc962091eef15a4e136febc04e3f
Peter Rhoades <pete@simpleweb.co.uk>
2013-07-11 15:06:00 +0100

    Adding quick class check to spec

--- spec/classes/opera_spec.rb
+++ spec/classes/opera_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'opera' do
   it do
+    should contain_class('opera')
     should contain_package('Opera').with({
       :provider => 'appdmg_eula',
       :source   => 'http://www.opera.com/download/get/?id=35909&location=360&nothanks=yes&sub=marine',
