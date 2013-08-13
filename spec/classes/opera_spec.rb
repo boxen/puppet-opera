28b5496426a34d026841df1d6a3d18cf2ff5d456
Peter Rhoades <pete@simpleweb.co.uk>
2013-08-13 22:20:51 +0100

    Using traditional direct download link to simplify module Will need to be kept updated

--- spec/classes/opera_spec.rb
+++ spec/classes/opera_spec.rb
@@ -1,24 +1,11 @@
 require 'spec_helper'
 
 describe 'opera' do
-  let(:facts) { default_test_facts }
-
   it do
     should contain_class('opera')
-
-    should contain_exec('Download Opera').with({
-      :command     => "/usr/bin/curl -o /tmp/opera.dmg -C - -k -L -s --url 'http://www.opera.com/download/get/?id=35909&location=360&nothanks=yes&sub=marine'",
-      :logoutput   => 'on_failure',
-      :creates     => '/var/db/.puppet_appdmg_installed_Opera'
-    })
-
     should contain_package('Opera').with({
       :provider => 'appdmg_eula',
-      :source   => '/tmp/opera.dmg'
-    })
-
-    should contain_file('/tmp/opera.dmg').with({
-      :ensure => 'absent'
+      :source   => 'http://get.geo.opera.com/pub/opera/desktop/15.0.1147.153/mac/Opera_15.0.1147.153_Setup.dmg'
     })
   end
 end
