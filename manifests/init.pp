b9d5d7052f8fd78e58cbf0b447e9495c64d9e883
Peter Rhoades <createdbypete@gmail.com>
2013-08-27 09:12:34 +0100

    Version bump Opera (16.0.1196.62)

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class opera {
   package { 'Opera':
     provider => 'appdmg_eula',
-    source   => 'http://get.geo.opera.com/pub/opera/desktop/15.0.1147.153/mac/Opera_15.0.1147.153_Setup.dmg'
+    source   => 'http://get.geo.opera.com/pub/opera/desktop/16.0.1196.62/mac/Opera_16.0.1196.62_Setup.dmg'
   }
 }
